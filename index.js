cbab8cd6ccba89c33be92dc32863760464c03e41
ThePeterBwire <peterbwirea@gmail.com>
2025-03-30 23:17:03 +0300

    pushes destructingAssignment

--- index.js
+++ index.js
@@ -46,3 +46,41 @@ const nestedMuppet = {
 // 7. Use destructuring to assign all variables using the keys as the variable names
 
 // 8. Use destructuring to assign songs 2 and 4, and Kermit's job and partner
+
+const farmAnimals = 'cow horse sheep pig chicken';
+const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+const muppet = { /* ... */ };
+const nestedMuppet = { /* ... */ };
+
+// Strings
+// 1. Destructure animal sounds
+const [moo, neigh, baa, oink, cluck] = farmAnimals.split(' ');
+
+// 2. Four animals (skip horse)
+const [bessie, , dolly, babe, little] = farmAnimals.split(' ');
+
+// 3. Three animals (skip horse and chicken)
+const [blackAndWhite, , black, pink] = farmAnimals.split(' ');
+
+// Arrays
+// 4. Assign color names
+const [red, orange, yellow, green, blue, indigo, violet] = colors;
+
+// 5. Skip indigo, use first letters
+const [r, o, y, g, b, , v] = colors;
+
+// 6. Assign indigo to `indg`
+const [,,,,, indg] = colors;
+
+// Objects
+// 7. Destructure muppet properties
+const { muppetName, color, song, job, partner } = muppet;
+
+// 8. Destructure nested songs and Kermit's details
+const { 
+  album: { 
+    theMuppetMovie: { song2, song4 } 
+  }, 
+  nestedJob, 
+  nestedPartner 
+} = nestedMuppet;
