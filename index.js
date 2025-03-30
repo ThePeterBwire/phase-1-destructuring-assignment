3c0a28eb57dd2ef8cce6b5adf14a8d2f6a89e93a
ThePeterBwire <peterbwirea@gmail.com>
2025-03-30 23:29:05 +0300

    pushes destructingAssignment

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const farmAnimals = 'cow horse sheep pig chicken';
+const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
 const muppet = {
   muppetName: 'Miss Piggy',
   color: 'pink',
@@ -5,16 +7,6 @@ const muppet = {
   job: 'Cast member of The Muppet Show',
   partner: 'Kermit'
 };
-
-// 7. Destructure all muppet properties
-const { 
-  muppetName, 
-  color, 
-  song, 
-  job, 
-  partner 
-} = muppet;
-
 const nestedMuppet = {
   nestedName: 'Kermit',
   nestedColor: 'green',
@@ -30,6 +22,36 @@ const nestedMuppet = {
   nestedPartner: 'Miss Piggy'
 };
 
+// Strings
+// 1. Destructure animal sounds
+const [moo, neigh, baa, oink, cluck] = farmAnimals.split(' ');
+
+// 2. Four animals (skip horse)
+const [bessie, , dolly, babe, little] = farmAnimals.split(' ');
+
+// 3. Three animals (skip horse and chicken)
+const [blackAndWhite, , black, pink] = farmAnimals.split(' ');
+
+// Arrays
+// 4. Assign color names
+const [red, orange, yellow, green, blue, indigo, violet] = colors;
+
+// 5. Skip indigo, use first letters
+const [r, o, y, g, b, , v] = colors;
+
+// 6. Assign indigo to `indg`
+const [,,,,, indg] = colors;
+
+// Objects
+// 7. Destructure all muppet properties
+const { 
+  muppetName, 
+  color, 
+  song, 
+  job, 
+  partner 
+} = muppet;
+
 // 8. Destructure nested songs and Kermit's details
 const { 
   album: { 
@@ -38,3 +60,9 @@ const {
   nestedJob,
   nestedPartner 
 } = nestedMuppet;
+
+
+
+
+
+
